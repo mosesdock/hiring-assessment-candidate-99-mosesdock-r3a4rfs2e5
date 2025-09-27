1d56af8ceff70a867869c2ef8447d87fe10ef750
mosesdock <idanmoshe16@gmail.com>
2025-09-27 12:42:44 +0300

    test1

--- wikijs-complete-terraform/terraform/environments/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-wikijs-543518525624"
-    key            = "wikijs/dev/terraform.tfstate"
-    region         = "il-central-1"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
-  }
-}
--- wikijs-complete-terraform/terraform/modules/monitoring/outputs.tf
+++
@@ -1,8 +0,0 @@
-# ============================================================================
-# terraform/modules/monitoring/outputs.tf
-# ============================================================================
-
-output "dashboard_url" {
-  description = "CloudWatch dashboard URL"
-  value       = "https://console.aws.amazon.com/cloudwatch/home?region=${data.aws_region.current.name}#dashboards:name=${aws_cloudwatch_dashboard.main.dashboard_name}"
-}
